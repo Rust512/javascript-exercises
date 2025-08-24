9851a9a1afb36203db378db81f310ebbc9194a83
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2025-08-24 07:16:01 +0530

    exercise-10: solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function () {
-
+const leapYears = function (year) {
+    if (year % 100 === 0) {
+        year = year / 100;
+    }
+    return year % 4 === 0;
 };
 
 // Do not edit below this line
