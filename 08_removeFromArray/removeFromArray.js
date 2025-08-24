b2f4164dee46550ace9d000708bc1e18640083d9
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2025-08-24 06:56:16 +0530

    exercise-08: solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function () {
+const removeFromArray = function (arr, ...values) {
+    let index = 0;
+    while (index < arr.length) {
+        if (values.includes(arr[index])) {
+            arr.splice(index, 1);
+            continue;
+        }
+        index++;
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
