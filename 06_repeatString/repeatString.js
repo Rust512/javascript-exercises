177c283d88ad669e4e78c029e07d1fc7b6ada2aa
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2025-08-24 06:31:37 +0530

    exercise-06: solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, frequency) {
+    if (!word) {
+        return '';
+    }
+    if (frequency < 0) {
+        return 'ERROR';
+    }
+    let result = '';
+    for (let i = 0; i < frequency; i++) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
